c9996075012b3f55cbd5e652d9b11e65dc394586
Artem Litvin <mafioznik445@gmail.com>
2024-03-26 11:05:43 +0200

    add redux-persist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import { Main } from "./pages/Main";
+//import {  } from "./axios-test-requests";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Main />
-        </ThemeProvider>
+        <PersistGate persistor={persistor} loading={null}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Main />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </div>
   );
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "modules/RegistrationForm";
 import { sidebarReducer } from "modules/Sidebar";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  sidebar: sidebarReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    sidebar: sidebarReducer,
-  },
+  reducer: persistedReducer,
   middleware(getDefaultMiddleware) {
     return getDefaultMiddleware({
       serializableCheck: false,
@@ -15,7 +26,9 @@ const store = configureStore({
   devTools: true,
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { persistor, store };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
