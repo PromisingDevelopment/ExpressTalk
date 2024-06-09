3fc59ed6a189005d19edf555a323ca5eaa9017c4
GlebHal <hammanov.gleb@gmail.com>
2024-06-09 14:04:52 +0300

    adjusted ClientChatMessageDto

--- backend/src/main/java/expresstalk/dev/backend/dto/ClientChatMessageDto.java
+++ backend/src/main/java/expresstalk/dev/backend/dto/ClientChatMessageDto.java
@@ -1,8 +1,10 @@
 package expresstalk.dev.backend.dto;
 
 import java.util.Date;
+import java.util.UUID;
 
 public record ClientChatMessageDto(
+        UUID senderId,
         String senderLogin,
         String content,
         Date createdAt
