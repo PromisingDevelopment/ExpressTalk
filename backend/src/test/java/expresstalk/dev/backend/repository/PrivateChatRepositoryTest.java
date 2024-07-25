107dad5a1095a06005c60eec8ba3957d2677e629
GlebHal <hammanov.gleb@gmail.com>
2024-07-25 17:28:20 +0300

    Added repositories unit tests

---
+++ backend/src/test/java/expresstalk/dev/backend/repository/PrivateChatRepositoryTest.java
@@ -0,0 +1,57 @@
+package expresstalk.dev.backend.repository;
+
+import expresstalk.dev.backend.entity.PrivateChat;
+import expresstalk.dev.backend.entity.PrivateChatAccount;
+import expresstalk.dev.backend.entity.User;
+import expresstalk.dev.backend.test_utils.TestValues;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PrivateChatRepositoryTest {
+    @Autowired
+    private PrivateChatRepository privateChatRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PrivateChatAccountRepository privateChatAccountRepository;
+
+    @AfterEach
+    void tearDown() {
+        privateChatRepository.deleteAll();
+        userRepository.deleteAll();
+        privateChatAccountRepository.deleteAll();
+    }
+
+    @Test
+    void shouldFindPrivateChatBetween() {
+        User user1 = TestValues.getUser();
+        User user2 = TestValues.getUser();
+        PrivateChat privateChat = new PrivateChat();
+        PrivateChatAccount account1 = new PrivateChatAccount();
+        PrivateChatAccount account2 = new PrivateChatAccount();
+
+        account1.setPrivateChat(privateChat);
+        account2.setPrivateChat(privateChat);
+        account1.setUser(user1);
+        account2.setUser(user2);
+        privateChat.getMembers().add(account1);
+        privateChat.getMembers().add(account2);
+        user1.getPrivateChatAccounts().add(account1);
+        user2.getPrivateChatAccounts().add(account2);
+
+        privateChatRepository.save(privateChat);
+        userRepository.saveAll(Arrays.asList(user1, user2));
+        privateChatAccountRepository.saveAll(Arrays.asList(account1, account2));
+
+        PrivateChat result = privateChatRepository.findPrivateChatBetween(user1, user2);
+        assertEquals(result.getId(), privateChat.getId());
+    }
+}
---
+++ backend/src/test/java/expresstalk/dev/backend/repository/UserRepositoryTest.java
@@ -0,0 +1,42 @@
+package expresstalk.dev.backend.repository;
+
+import expresstalk.dev.backend.entity.User;
+import expresstalk.dev.backend.test_utils.TestValues;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void shouldFindUserByLoginOrEmail() {
+        User user = TestValues.getUser();
+        userRepository.save(user);
+
+        User result = userRepository.findUserByLoginOrEmail(user.getLogin(), user.getEmail());
+        assertEquals(result.getLogin(), user.getLogin());
+        result = userRepository.findUserByLoginOrEmail(user.getLogin(), user.getLogin());
+        assertEquals(result.getLogin(), user.getLogin());
+        result = userRepository.findUserByLoginOrEmail(user.getEmail(), user.getEmail());
+        assertEquals(result.getLogin(), user.getLogin());
+    }
+    @Test
+    void shouldFindUserByLogin() {
+        User user = TestValues.getUser();
+        userRepository.save(user);
+
+        User result = userRepository.findUserByLogin(user.getLogin());
+        assertEquals(result.getLogin(), user.getLogin());
+    }
+}
